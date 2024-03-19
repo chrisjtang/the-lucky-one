4abd297678ddc7acd2db6ac96d4bc342d9dcd82e
Chris <chrisjtang@gmail.com>
2024-03-19 15:54:59 -0700

    add quote generation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
+interface QuoteType {
+    text: string;
+    from: string;
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [allQuotes, setAllQuotes] = useState([]);
+  const [quote, setQuote] = useState<QuoteType>();
 
   useEffect(() => {
-    (function() {
+    (function():void {
       const d = document, s = d.createElement('script');
       s.src = 'https://ctang.disqus.com/embed.js';
       s.setAttribute('data-timestamp', String(+new Date()));
@@ -13,17 +19,43 @@ function App() {
     })();
   }, []);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('../data/quotes.json');
+        const data = await response.json();
+        setAllQuotes(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []);
+
+// grab random quote + remove quote from allQuotes
+  const handleGenerateRandomQuote = () => {
+    const randomIndex = Math.floor(Math.random() * allQuotes.length);
+    const randomQuote = allQuotes[randomIndex];
+    
+    // swap the random quote with the last quote in the list.  
+    // pop the random quote off.
+    const lastQuote = allQuotes[allQuotes.length - 1];
+    allQuotes[randomIndex] = lastQuote;
+    allQuotes[allQuotes.length - 1] = randomQuote;
+    setQuote(allQuotes.pop());
+  }
 
   return (
     <>
       <h1>The Lucky One</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={handleGenerateRandomQuote}>
+          {allQuotes.length ? quote ? 'AND ANOTHA ONE!' : `I'm feeling lucky` : `That's it!`}
         </button>
-        <p>
-          How high did you get?
-        </p>
+        <div className='quote'>
+            <h3>{quote ? `"${quote.text}"` : ''}</h3>
+            <p>{quote ? `${quote.from}` : '' }</p>
+        </div>
       </div>
       <div id="disqus_thread"></div>
     <noscript>
